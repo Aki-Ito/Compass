98258ca5d4321a4ae901aa8b6c1c58f21d8f8cb6
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-05-11 15:18:42 +0900

    Initial Commit

---
+++ SC/SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCApp.swift
+//  SC
+//
+//  Created by 伊藤明孝 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
