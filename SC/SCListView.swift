7d9dc78af5c2c34325a92b8b02b2461033c9017f
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-05-13 23:54:18 +0900

    finished making list layout

--- SC/SCListView.swift
+++ SC/SCListView.swift
@@ -14,7 +14,30 @@ struct SCListView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                Color(.red).edgesIgnoringSafeArea(.bottom)
+                //MARK: backgroundView
+                LinearGradient(colors: [Color( "BG1"),Color("BG2")], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                GeometryReader { geometryProxy in
+                    let size = geometryProxy.size
+                    
+                    Color.white.opacity(0.7)
+                        .blur(radius: 200)
+                        .ignoresSafeArea()
+                    
+                    Circle()
+                        .fill(Gradient(colors: [Color("CirclePink2"),Color("CirclePink1")]))
+                        .blur(radius: 20)
+                        .frame(width: 300, height: 300)
+                        .offset(x: 240,y: -10)
+                    
+                    Circle()
+                        .fill(Gradient(colors: [Color("CirclePink3"),Color("CirclePink4")]))
+                        .blur(radius: 20)
+                        .frame(width: 300, height: 300)
+                        .offset(x: -170,y: 240)
+                }
+                
                 //MARK: ListView
                 ScrollView {
                     LazyVGrid(columns: columns) {
@@ -31,7 +54,14 @@ struct SCListView: View {
                                     .background(
                                         RoundedRectangle(cornerRadius: 25)
                                             .stroke(
-                                                
+                                                .linearGradient(.init(colors: [
+                                                    Color("CirclePink1"),
+                                                    Color("CirclePink1").opacity(0.5),
+                                                    .clear,
+                                                    .clear,
+                                                    Color("CirclePink2"),
+                                                    ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                                ,lineWidth: 2.5
                                             )
                                             .padding(2)
                                     )
