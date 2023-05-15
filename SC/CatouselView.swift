fe87fb23115383162a9551767d919b4ea5df39ac
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-05-15 16:21:01 +0900

    add files

---
+++ SC/CatouselView.swift
@@ -0,0 +1,20 @@
+//
+//  CatouselView.swift
+//  SC
+//
+//  Created by 伊藤明孝 on 2023/05/14.
+//
+
+import SwiftUI
+
+struct CatouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatouselView()
+    }
+}
---
+++ SC/FeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeaturedView.swift
+//  SC
+//
+//  Created by 伊藤明孝 on 2023/05/14.
+//
+
+import SwiftUI
+
+struct FeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedView()
+    }
+}
