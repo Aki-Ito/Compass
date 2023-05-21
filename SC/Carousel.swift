de7c55eedaa46c2e9883928bbb481ff2afd1c976
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-05-21 22:36:10 +0900

    Carousel View created

---
+++ SC/Carousel.swift
@@ -0,0 +1,156 @@
+//
+//  Carousel.swift
+//  SC
+//
+//  Created by 伊藤明孝 on 2023/05/21.
+//
+
+import SwiftUI
+
+//MARK: CustomView
+struct Carousel<Content: View,Item,ID>: View where Item: RandomAccessCollection,ID: Hashable, Item.Element: Equatable{
+    var content: (Item.Element, CGSize)->Content
+    var id: KeyPath<Item.Element,ID>
+    
+    //MARK: View Properties
+    var spacing: CGFloat
+    var cardPadding: CGFloat
+    var items: Item
+    @Binding var index: Int
+    
+    init(index: Binding<Int>,items: Item,spacing: CGFloat = 30, cardPadding: CGFloat = 80, id: KeyPath<Item.Element, ID>,content: @escaping (Item.Element, CGSize) -> Content) {
+        self.content = content
+        self.id = id
+        self.spacing = spacing
+        self.cardPadding = cardPadding
+        self.items = items
+        self._index = index
+    }
+    //MARK: Gesture properties
+    @GestureState var translation: CGFloat = 0
+    @State var offset: CGFloat = 0
+    @State var lastStoredOffset: CGFloat = 0
+    @State var currentIndex: Int = 0
+    
+    //MARK: Rotation
+    @State var rotation: Double = 0
+    
+    var body: some View {
+        GeometryReader { proxy in
+            let size = proxy.size
+            
+            let cardWidth = size.width - (cardPadding - spacing)
+            LazyHStack(spacing: spacing){
+                ForEach(items, id: id){movie in
+                    let index = indexOf(item: movie)
+                    content(movie, CGSize(width: size.width - cardPadding, height: size.height))
+                    
+                    //MARK: Rotating Each View 5 Deg Multiplied With it's Index
+                    //All While Scrolling Setting it to 0, thus it will give some nice Circular Carousel Effect
+                        .rotationEffect(.init(degrees: Double(index) * 5),anchor: .bottom)
+                        .rotationEffect(.init(degrees: rotation), anchor: .bottom)
+                    //MARK: Apply After Rotation, Thus you will get smooth effect
+                        .offset(y: offsetY(index: index, cardWidth: cardWidth))
+                        .frame(width: size.width - cardPadding, height: size.height)
+                        .contentShape(Rectangle())
+                }
+            }
+            .padding(.horizontal,spacing)
+            .offset(x: limitScroll())
+            .contentShape(Rectangle())
+            .gesture(
+                DragGesture(minimumDistance: 5)
+                    .updating($translation, body: { value, out, _ in
+                        out = value.translation.width
+                    })
+                    .onChanged{onChanged(value:$0, cardWidth: cardWidth)}
+                    .onEnded{onEnd(value:$0, cardWidth: cardWidth)}
+            )
+        }
+        .padding(.top,60)
+        .onAppear{
+            let extraSpace = (cardPadding/2) - spacing
+            offset = extraSpace
+            lastStoredOffset = extraSpace
+        }
+        .animation(.easeInOut, value: translation == 0)
+    }
+    
+    //MARK: Moving Current Item Up
+    func offsetY(index: Int, cardWidth: CGFloat) -> CGFloat{
+        //MARK: We are converting the current translation, not whole offset
+        //That's why created @Gesturestate to hold the current translation data
+        //Converting translation to -60 ... 60
+        let progress = ((translation < 0 ? translation : -translation)/cardWidth) * 60
+        let yOffset = -progress < 60 ? progress : -(progress + 120)
+        
+        //MARK: Checking previous, next and in-between offsets
+        let previous = (index - 1) == self.index ? (translation < 0 ? yOffset : -yOffset) : 0
+        let next = (index + 1) == self.index ? (translation < 0 ? -yOffset : yOffset) : 0
+        let In_Between = (index - 1) == self.index ? previous:next
+        
+        return index == self.index ? -60 - yOffset : In_Between
+    }
+    
+    //MARK: Item Index
+    func indexOf(item: Item.Element) -> Int{
+        let array = Array(items)
+        if let index = array.firstIndex(of: item){
+            return index
+        }
+        return 0
+    }
+    
+    //MARK: Limiting Scroll On First And Last Items
+    func limitScroll ()-> CGFloat{
+        let extraSpace = (cardPadding/2) - spacing
+        if index == 0 && offset > extraSpace{
+            return offset/4
+        }else if index == items.count - 1 && translation < 0{
+            return offset - (translation/2)
+        }else{
+            return offset
+        }
+    }
+    
+    func onChanged(value: DragGesture.Value, cardWidth: CGFloat){
+        let translationX = value.translation.width
+        offset = translationX + lastStoredOffset
+        
+        //MARK: Caluculating Rotation
+        let progress = offset / cardWidth
+        rotation = progress * 5
+    }
+    
+    func onEnd(value: DragGesture.Value, cardWidth: CGFloat){
+        // MARK: Finding Current Index
+        var _index = (offset/cardWidth).rounded()
+        _index = max(-CGFloat(items.count - 1), _index)
+        _index = min(_index, 0)
+        
+        currentIndex = Int(_index)
+        //MARK: updating Index
+        //Note Since We're Moving On Right Side
+        //So All Data will be Negative
+        index = -currentIndex
+        withAnimation(.easeInOut(duration: 0.25)){
+            //MARK: Removing extra space
+            let extraSpace = (cardPadding/2) - spacing
+            offset = (cardWidth * _index) + extraSpace
+            
+            //MARK: Caluculating Rotation
+            let progress = offset / cardWidth
+            //Since Index Starts with zero
+            //エラーがある気がする
+            rotation = (progress * 5).rounded() - 1
+            print(rotation)
+        }
+        lastStoredOffset = offset
+    }
+}
+
+struct Carousel_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedView()
+    }
+}
--- SC/FeaturedView.swift
+++ SC/FeaturedView.swift
@@ -8,11 +8,127 @@
 import SwiftUI
 
 struct FeaturedView: View {
+    @State var currentTab: Tab = .home
+    @Namespace var animation
+    
+    @State var currentIndex: Int = 0
+    
     var body: some View {
-        VStack{
+        VStack(spacing: 15){
+            HeaderView()
+            SearchBar()
             
-        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-            .background()
+            //MARK: Custom Carousel
+            Carousel(index: $currentIndex, items: movies, cardPadding: 150, id: \.id) { movie,cardSize in
+                
+                Image(movie.artwork)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: cardSize.width,height: cardSize.height)
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            }
+            .padding(.horizontal,-15)
+            .padding(.vertical)
+            
+            TabBar()
+        }
+        .padding([.horizontal, .top],15)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .background{
+                GeometryReader { proxy in
+                    LinearGradient(colors: [Color("BG1"),Color("BG2")], startPoint: .top, endPoint: .bottom)
+                }.ignoresSafeArea()
+            }
+    }
+    
+    //MARK: TabBar
+    @ViewBuilder
+    func TabBar() -> some View{
+        HStack(spacing: 0){
+            ForEach(Tab.allCases, id: \.rawValue){tab in
+                VStack(spacing: -2) {
+                    Image(tab.rawValue)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 28,height: 28)
+                        .foregroundColor(currentTab == tab ? .white : .gray.opacity(0.6))
+                    
+                    if currentTab == tab{
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 5, height: 6)
+                            .offset(y: 10)
+                            .matchedGeometryEffect(id: "TAB", in: animation)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    withAnimation(.easeInOut){currentTab = tab}
+                }
+            }
+        }
+        .padding(.horizontal)
+        .padding(.bottom, 10)
+    }
+    
+    //MARK: SearchBar
+    @ViewBuilder
+    func SearchBar() -> some View{
+        HStack(spacing: 15) {
+            Image("Search")
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 28, height: 28)
+                .foregroundColor(.gray)
+            
+            TextField("Search", text: .constant(""))
+                .padding(.vertical, 10)
+            
+            Image("Mic")
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 28, height: 28)
+                .foregroundColor(.gray)
+        }
+        .padding(.horizontal)
+        .padding(.vertical,6)
+        .background {
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color.white.opacity(0.2))
+        }
+        .padding(.top,20)
+    }
+    
+    //MARK: HeaderView
+    @ViewBuilder
+    func HeaderView() -> some View{
+        HStack {
+            VStack(alignment: .leading, spacing: 6) {
+                (Text("Hello")
+                    .fontWeight(.semibold) +
+                Text("Aki")
+                ).font(.title2)
+                
+                Text("see featured solution")
+                    .font(.callout)
+                    .foregroundColor(.gray)
+
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            
+            Button {
+                print("")
+            } label: {
+                Image("")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+            }
+        }
     }
 }
 
